6d6179f5b5f7776ea8d1f3ad09298366cc4b5fdc
tim <tim@awaken.finance>
2024-01-25 20:25:22 +0800

    fix: 🐛 add decimal

--- src/Modals/ManageLiquidityModal/HoldLiquidity.tsx
+++ src/Modals/ManageLiquidityModal/HoldLiquidity.tsx
@@ -36,6 +36,11 @@ export default function HoldLiquidity({
   console.log('lp count :', lp);
 
   const showLp = divDecimals(lp, getLPDecimals());
+  const lpUnit = useMemo(() => {
+    const uLP = unitConverter(showLp, 8);
+    if (uLP === '0') return '0.00';
+    return uLP;
+  }, [showLp]);
 
   const leftLPBalance = useMemo(() => {
     const reserveL = reserves?.[getCurrencyAddress(leftToken)];
@@ -86,7 +91,7 @@ export default function HoldLiquidity({
             </Col>
             <Col>
               <Font lineHeight={24} size={16} weight="medium">
-                {unitConverter(showLp)}
+                {lpUnit}
               </Font>
             </Col>
           </Row>
