b269b6b406968f506be4f2d74aa5dcda1d514613
tim <tim@awaken.finance>
2024-04-22 14:30:12 +0800

    style: 💄 loading style

--- src/components/CommonLoading/loading.tsx
+++ src/components/CommonLoading/loading.tsx
@@ -3,12 +3,12 @@ import { LoadingBg } from 'assets/icons';
 import Lottie from 'lottie-react';
 import './index.less';
 
-export const COMMON_LOADING_CONFIG = {
+export const COMMON_LOADING_CONFIG = (showBg = false) => ({
   wrapperClassName: 'common-loading',
   indicator: (
     <span className="loading-box">
       <Lottie className="loading-box-animation" animationData={awakenLoading} loop />
-      <LoadingBg className="loading-box-bg" />
+      {showBg && <LoadingBg className="loading-box-bg" />}
     </span>
   ),
-};
+});
