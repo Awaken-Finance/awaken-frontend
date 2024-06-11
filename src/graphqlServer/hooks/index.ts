65b3fc71b08737c226df244218fe1afda2436985
harold-awaken <harold@awaken.finance>
2024-06-12 00:55:41 +0800

    fix: 🐛 adjust Awaken GraphQLUrl

--- src/graphqlServer/hooks/index.ts
+++ src/graphqlServer/hooks/index.ts
@@ -3,8 +3,20 @@ import { getGraphQLClient } from '../client';
 import { useCallback } from 'react';
 import { TGraphQLParamsType } from '../types';
 
+const { NODE_ENV, REACT_APP_API_ENV } = process.env;
+const AWAKEN_GRAPHQL_URL_MAP: Record<string, string> = {
+  preview: 'https://test-dapp.awaken.finance/AElfIndexer_Swap/SwapIndexerSchema/graphql',
+  test: 'https://test-dapp.awaken.finance/AElfIndexer_Swap/SwapIndexerSchema/graphql',
+  mainNet: 'https://dapp.awaken.finance/AElfIndexer_Swap/SwapIndexerSchema/graphql',
+};
+
+const AWAKEN_GRAPHQL_URL =
+  NODE_ENV !== 'production'
+    ? '/AElfIndexer_Swap/SwapIndexerSchema/graphql'
+    : AWAKEN_GRAPHQL_URL_MAP[REACT_APP_API_ENV || ''] || AWAKEN_GRAPHQL_URL_MAP.mainNet;
+
 export const useGraphQLClient = () => {
-  return getGraphQLClient('/AElfIndexer_Swap/SwapIndexerSchema/graphql');
+  return getGraphQLClient(AWAKEN_GRAPHQL_URL);
 };
 
 export const useGetPairSyncRecords = () => {
