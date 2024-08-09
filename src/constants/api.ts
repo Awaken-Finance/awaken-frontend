38590a0b9deb0e8b8b035c9da53ae00cb3194f7d
harold-awaken <harold@awaken.finance>
2024-08-09 17:19:07 +0800

    feat: 🎸 adjust api host

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,10 @@
 export const PROD_API: { [env: string]: string } = {
-  test: '',
+  test: 'https://test-app.awaken.finance',
   preview: 'https://test-app.awaken.finance',
+};
+
+export const PROD_CMS: { [env: string]: string } = {
+  prod: 'https://cms-v2.awaken.finance',
+  test: 'https://test-cms-v2.awaken.finance',
+  preview: 'https://test-cms-v2.awaken.finance',
 };
