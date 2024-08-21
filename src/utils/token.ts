ad013b3f44e61ce2cc6f6cd8eb55a0de8349cd3f
harold-awaken <harold@awaken.finance>
2024-08-21 12:09:40 +0800

    feat: 🎸 adapts NFT Image

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,4 +1,4 @@
-import { SYMBOL_FORMAT_MAP } from 'constants/misc';
+import { IPFS_SYMBOL_LIST, IPFS_SYMBOL_MAP, SYMBOL_FORMAT_MAP } from 'constants/misc';
 
 const tokenWeights: { [key: string]: number } = {
   USDT: 100,
@@ -33,4 +33,26 @@ export const formatSymbol = (symbol = '') => {
 export const getTVSymbolName = (pathSymbol = '') => {
   const arr = pathSymbol.split('_');
   return arr.map((item) => formatSymbol(item)).join('/');
+};
+
+export const formatImageURI = (symbol: string, uri: string) => {
+  const isIPFS = (uri || '').startsWith('ipfs://');
+  if (!isIPFS) return uri;
+  return getIPFSAssetsURI(symbol, uri);
+};
+
+export const getIPFSAssetsURI = (symbol = '', uri: string) => {
+  const symbolPrefix = symbol.split('-')?.[0] || '';
+  return `${IPFS_SYMBOL_MAP[symbolPrefix]}/${uri.slice(7)}`;
+};
+
+export const getImageFromTokenInfo = (tokenInfo: any) => {
+  const symbol = tokenInfo?.symbol || '';
+  const symbolPrefix = symbol.split('-')?.[0] || '';
+
+  if (IPFS_SYMBOL_LIST.includes(symbolPrefix)) {
+    return formatImageURI(symbol, tokenInfo?.externalInfo?.value?.__nft_image_uri || '');
+  } else {
+    return tokenInfo?.externalInfo?.value?.__ft_image_uri || tokenInfo?.externalInfo?.value?.__nft_image_url || '';
+  }
 };
