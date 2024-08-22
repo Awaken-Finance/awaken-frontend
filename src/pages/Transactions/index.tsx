cc951ee73f886f9a02fffbe4bc628cb27a281a26
harold-awaken <harold@awaken.finance>
2024-08-22 11:49:36 +0800

    feat: 🎸 trade list route optimization

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -101,9 +101,9 @@ export default function Transactions() {
       getList(pageInfo.current, '', val);
       setSearchVal('');
       if (val === TranslationMenuEnum.trade) {
-        history.push(`/transactions`);
+        history.replace(`/transactions`);
       } else {
-        history.push(`/transactions/${TranslationMenuEnum[val as any]}`);
+        history.replace(`/transactions/${TranslationMenuEnum[val as any]}`);
       }
     },
     [getList, history],
