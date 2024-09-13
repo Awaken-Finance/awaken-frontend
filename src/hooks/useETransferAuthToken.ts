6749a3219bd2d8521e25cda7b50c43e5f05491b6
harold-awaken <harold@awaken.finance>
2024-09-13 17:28:45 +0800

    feat: 🎸 deposit userInfo cache optimization

--- src/hooks/useETransferAuthToken.ts
+++ src/hooks/useETransferAuthToken.ts
@@ -6,11 +6,12 @@ import { AuthTokenSource, PortkeyVersion } from '@etransfer/types';
 import { ETransferConfig, getETransferReCaptcha, WalletTypeEnum, etransferCore } from '@etransfer/ui-react';
 import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
 
-import { getCaHashAndOriginChainIdByWallet, getManagerAddressByWallet } from 'utils/wallet';
+import { getManagerAddressByWallet } from 'utils/wallet';
 import { useIsConnected } from './useLogin';
 import { APP_NAME } from 'config/webLoginConfig';
 import { TWalletType } from '@etransfer/types';
 import { useGetAccount } from './wallet';
+import { getETransferUserInfo } from 'utils/etransfer';
 
 export type TSetETransferConfigParams = {
   managerAddress: string;
@@ -53,7 +54,7 @@ export function useETransferAuthToken() {
     if (!isLoginRef.current) throw new Error('You are not logged in.');
 
     try {
-      const { caHash, originChainId } = await getCaHashAndOriginChainIdByWallet(walletInfo, walletType);
+      const { caHash, originChainId } = await getETransferUserInfo(walletInfo, walletType);
       // TODO: cache caHash & originChainId
       const managerAddress = await getManagerAddressByWallet(walletInfo, walletType);
       console.log('etransferInfo', {
