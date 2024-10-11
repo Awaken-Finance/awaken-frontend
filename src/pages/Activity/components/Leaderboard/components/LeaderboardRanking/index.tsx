18925a4427859d5beebadc3b052d468f79826ffd
harold-awaken <harold@awaken.finance>
2024-10-11 16:05:40 +0800

    feat: 🎸 activityList optimization

--- src/pages/Activity/components/Leaderboard/components/LeaderboardRanking/index.tsx
+++ src/pages/Activity/components/Leaderboard/components/LeaderboardRanking/index.tsx
@@ -3,7 +3,7 @@ import './styles.less';
 import { ActivityStatusEnum } from 'pages/Activity/hooks/common';
 import { LeaderboardRankingMine } from '../LeaderboardRankingMine';
 import { LeaderboardRankingList } from '../LeaderboardRankingList';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { getActivityMyRanking, getActivityRankingList } from 'api/utils/activity';
 import { TLeaderboardRankingItem, TLeaderboardRankingMine } from 'types/activity';
 import { useConnectWallet } from '@aelf-web-login/wallet-adapter-react';
@@ -24,6 +24,7 @@ export const LeaderboardRanking = ({ activity, status }: TLeaderboardCountdownPr
       setMyRankingInfo(undefined);
       return;
     }
+
     try {
       const result = await getActivityMyRanking({
         activityId: Number(activity.serviceId || 0),
@@ -34,6 +35,8 @@ export const LeaderboardRanking = ({ activity, status }: TLeaderboardCountdownPr
       console.log('initMyRankingInfo error', error);
     }
   }, [activity.serviceId, walletInfo?.address]);
+  const initMyRankingInfoRef = useRef(initMyRankingInfo);
+  initMyRankingInfoRef.current = initMyRankingInfo;
 
   useEffect(() => {
     initMyRankingInfo();
@@ -57,20 +60,40 @@ export const LeaderboardRanking = ({ activity, status }: TLeaderboardCountdownPr
     initRankingList();
   }, [initRankingList]);
 
-  const { list: wsList } = useLeaderboardWS(isInit, activity.serviceId || 0);
+  const { list: wsList } = useLeaderboardWS({
+    isInit: isInit,
+    activityId: activity.serviceId || 0,
+  });
   const list = useMemo(() => wsList || apiList || [], [apiList, wsList]);
+
+  const rankingInfoFromList = useMemo(
+    () => list.find((item) => item.address === walletInfo?.address),
+    [list, walletInfo?.address],
+  );
+  const rankingInfo = useMemo(() => rankingInfoFromList || myRankingInfo, [myRankingInfo, rankingInfoFromList]);
+
+  const isWsInitRef = useRef(false);
+  useEffect(() => {
+    if (!wsList) return;
+    if (isWsInitRef.current === false) {
+      isWsInitRef.current = true;
+      return;
+    }
+    console.log('wsList update');
+    initMyRankingInfoRef.current();
+  }, [wsList]);
 
   if (status === ActivityStatusEnum.Preparation) return <></>;
 
   return (
     <div className="leaderboard-ranking">
-      {myRankingInfo && (
+      {rankingInfo && (
         <LeaderboardRankingMine
           className="leaderboard-ranking-mine-section"
           activity={activity}
           status={status}
           list={list}
-          info={myRankingInfo}
+          info={rankingInfo}
         />
       )}
 
